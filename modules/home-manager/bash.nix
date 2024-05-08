9c03accb095e33aacf50e35adc790ac1c26ab87d
Joshua Smart <josh@thesmarts.co.uk>
2024-05-08 22:58:11 +0100

    35 current  2024-05-08 22:56:07  23.11.20240506.27c1399   6.1.90                          *

--- modules/home-manager/bash.nix
+++ modules/home-manager/bash.nix
@@ -6,7 +6,7 @@
       "la" = "ls -aF";
       "ll" = "ls -alFh";
     };
-    initExtra =
-      "PS1=[033[38;5;41m]u@h[$(tput sgr0)]:[$(tput sgr0)][033[38;5;33m]W[$(tput sgr0)] [$(tput sgr0)][033[38;5;33m]\\\\$[$(tput sgr0)] [$(tput sgr0)]";
+    bashrcExtra =
+      "PS1=[033[38;5;41m]u@h[$(tput sgr0)]:[$(tput sgr0)][033[38;5;33m]W[$(tput sgr0)] [$(tput sgr0)][033[38;5;33m]\\$[$(tput sgr0)] [$(tput sgr0)]";
   };
 }
