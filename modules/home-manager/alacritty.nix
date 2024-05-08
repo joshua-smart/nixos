4c33b3c3d4414ae46451ba9457b373e263ec4ff3
Joshua Smart <josh@thesmarts.co.uk>
2024-05-08 22:45:50 +0100

    34 current  2024-05-08 22:45:46  23.11.20240506.27c1399   6.1.90                          *

--- modules/home-manager/alacritty.nix
+++ modules/home-manager/alacritty.nix
@@ -9,6 +9,37 @@
         opacity = 0.6;
         title = "Terminal";
       };
+      colors = {
+        # Default colors
+        primary = {
+          background = "#2D2A2E";
+          foreground = "#fff1f3";
+        };
+
+        # Normal colors
+        normal = {
+          black = "#2c2525";
+          red = "#fd6883";
+          green = "#adda78";
+          yellow = "#f9cc6c";
+          blue = "#f38d70";
+          magenta = "#a8a9eb";
+          cyan = "#85dacc";
+          white = "#fff1f3";
+        };
+
+        # Bright colors
+        bright = {
+          black = "#72696a";
+          red = "#fd6883";
+          green = "#adda78";
+          yellow = "#f9cc6c";
+          blue = "#f38d70";
+          magenta = "#a8a9eb";
+          cyan = "#85dacc";
+          white = "#fff1f3";
+        };
+      };
     };
   };
 }
