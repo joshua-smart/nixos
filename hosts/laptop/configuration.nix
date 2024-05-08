872c7e74c7e00c148d9b65f366f212961453d8c5
Joshua Smart <josh@thesmarts.co.uk>
2024-05-08 22:30:15 +0100

    28 current  2024-05-08 22:30:12  23.11.20240506.27c1399   6.1.90                          *

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -34,7 +34,7 @@
   # hardware = { opengl.enable = true; };
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
 
   # Enable CUPS to print documents.
