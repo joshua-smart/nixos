bb5553fd3bbb3c524065efe3ca8f4e5de0b2681c
Joshua Smart <josh@thesmarts.co.uk>
2024-05-09 06:52:16 +0100

    60 current  2024-05-09 06:52:12  23.11.20240506.27c1399   6.1.90                          *

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -23,11 +23,13 @@
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
+  fonts.fontconfig.enable = true;
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
     discord
     tree
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
