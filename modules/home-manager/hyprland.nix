fca06e4836f28b96b5ad94839319aa5969e34f20
Joshua Smart <josh@thesmarts.co.uk>
2024-05-09 03:41:15 +0100

    42 current  2024-05-09 03:41:11  23.11.20240506.27c1399   6.1.90                          *

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -23,8 +23,8 @@
 
       border_size = 2;
 
-      col.active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-      # col.inactive_border = "rgba(595959aa)";
+      "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+      "col.inactive_border" = "rgba(595959aa)";
 
       resize_on_border = false;
 
