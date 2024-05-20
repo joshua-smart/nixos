6ce92697bb8fe3760e8c51bb3eba41bd7e2f418b
Joshua Smart <josh@thesmarts.co.uk>
2024-05-20 21:33:32 +0100

    Factored out sddm config

--- nixosModules/display/default.nix
+++ nixosModules/display/default.nix
@@ -1,14 +1,11 @@
-{ pkgs, ... }: {
+{ ... }: {
+  imports = [ ./sddm.nix ];
+
   services.xserver.enable = true;
+
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-  };
-
-  services.xserver.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-    theme = "${import ./sddm-sugar-dark-theme.nix { inherit pkgs; }}";
   };
 
   programs.light.enable = true;
---
+++ nixosModules/display/sddm.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }: {
+  services.xserver.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    theme = "${import ./sddm-sugar-dark-theme.nix { inherit pkgs; }}";
+  };
+
+  environment.systemPackages = with pkgs; [
+    libsForQt5.qt5.qtquickcontrols2
+    libsForQt5.qt5.qtgraphicaleffects
+  ];
+}
