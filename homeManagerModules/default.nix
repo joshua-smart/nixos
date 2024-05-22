5808855eb2639315317e3776f50231da9b0cc337
Joshua Smart <josh@thesmarts.co.uk>
2024-05-22 01:43:03 +0100

    Added nmapplet service

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -1,1 +1,1 @@
-{ ... }: { imports = [ ./display ./programs ./fonts.nix ]; }
+{ ... }: { imports = [ ./display ./programs ./services ./fonts.nix ]; }
---
+++ homeManagerModules/services/default.nix
@@ -0,0 +1,1 @@
+{ ... }: { imports = [ ./network-manager-applet.nix ]; }
