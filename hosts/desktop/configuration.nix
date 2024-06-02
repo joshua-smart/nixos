523377be3eac31dc84d164713ec2c8de56c1ae4c
Joshua Smart <josh@thesmarts.co.uk>
2024-06-02 18:35:19 +0100

    Updated to nvidia-555 driver

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ ... }:
+{ config, pkgs-unstable, ... }:
 
 {
   imports = [ # Include the results of the hardware scan.
@@ -11,5 +11,9 @@
   ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    package = (pkgs-unstable.linuxPackagesFor
+      config.boot.kernelPackages.kernel).nvidiaPackages.beta;
+  };
 }
