15e31406f52757d1cd90a4ba41f1da8c8afb9416
Joshua Smart <josh@thesmarts.co.uk>
2024-06-02 18:34:52 +0100

    Fixed missing file picker portal

--- homeManagerModules/display/default.nix
+++ homeManagerModules/display/default.nix
@@ -1,3 +1,8 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [ ./waybar ./hyprland.nix ./hyprpaper ./cursor.nix ./gtk.nix ];
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  };
 }
