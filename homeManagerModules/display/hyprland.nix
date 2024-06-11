b80b701020d4f8e6145653a2cbce384a7803fe74
Joshua Smart <josh@thesmarts.co.uk>
2024-06-11 19:32:31 +0100

    Setup waybar as systemd service

--- homeManagerModules/display/hyprland.nix
+++ homeManagerModules/display/hyprland.nix
@@ -19,168 +19,172 @@ in {
 
   config = {
 
-    wayland.windowManager.hyprland.enable = true;
-    wayland.windowManager.hyprland.settings = {
-      # MONITORS
-      monitor = cfg.monitors;
+    wayland.windowManager.hyprland = {
+      enable = true;
+      systemd.enable = true;
 
-      # AUTOSTART
-      exec-once = [ "${pkgs.waybar}/bin/waybar" ];
+      settings = {
+        # MONITORS
+        monitor = cfg.monitors;
 
-      # ENVIRONMENT VARIABLES
-      env = optionals cfg.nvidia [ "WLR_NO_HARDWARE_CURSORS,1" ];
+        # AUTOSTART
+        exec-once = [ ];
 
-      # LOOK AND FEEL
-      general = {
-        gaps_in = 5;
-        gaps_out = 10;
+        # ENVIRONMENT VARIABLES
+        env = optionals cfg.nvidia [ "WLR_NO_HARDWARE_CURSORS,1" ];
 
-        border_size = 2;
+        # LOOK AND FEEL
+        general = {
+          gaps_in = 5;
+          gaps_out = 10;
 
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        "col.inactive_border" = "rgba(595959aa)";
+          border_size = 2;
 
-        resize_on_border = false;
+          "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+          "col.inactive_border" = "rgba(595959aa)";
 
-        allow_tearing = false;
+          resize_on_border = false;
 
-        layout = "master";
+          allow_tearing = false;
 
-        cursor_inactive_timeout = 1;
-      };
+          layout = "master";
 
-      decoration = {
-        rounding = 10;
-
-        active_opacity = 1.0;
-        inactive_opacity = 1.0;
-
-        drop_shadow = true;
-        shadow_range = 4;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(1a1a1aee)";
-
-        blur = {
-          enabled = true;
-          size = 8;
-          passes = 1;
-
-          vibrancy = 0.1696;
+          cursor_inactive_timeout = 1;
         };
-      };
 
-      animations = {
-        enabled = true;
+        decoration = {
+          rounding = 10;
 
-        # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+          active_opacity = 1.0;
+          inactive_opacity = 1.0;
 
-        animation = [
-          "windows, 1, 5, default"
-          "windowsOut, 1, 5, default, popin 80%"
-          "border, 1, 10, default"
-          "borderangle, 1, 5, default"
-          "fade, 1, 5, default"
-          "workspaces, 1, 5, default"
-        ];
-      };
+          drop_shadow = true;
+          shadow_range = 4;
+          shadow_render_power = 3;
+          "col.shadow" = "rgba(1a1a1aee)";
 
-      master = {
-        new_is_master = true;
-        no_gaps_when_only = 1;
-      };
+          blur = {
+            enabled = true;
+            size = 8;
+            passes = 1;
 
-      misc = {
-        force_default_wallpaper = 0;
-        disable_hyprland_logo = true;
-        new_window_takes_over_fullscreen = 1;
-      };
+            vibrancy = 0.1696;
+          };
+        };
 
-      # INPUT
-      input = {
-        kb_layout = "gb";
-        follow_mouse = 1;
-        sensitivity = 0;
+        animations = {
+          enabled = true;
 
-        touchpad.natural_scroll = false;
-      };
+          # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
-      gestures = {
-        workspace_swipe = true;
-        workspace_swipe_fingers = 3;
-      };
+          animation = [
+            "windows, 1, 5, default"
+            "windowsOut, 1, 5, default, popin 80%"
+            "border, 1, 10, default"
+            "borderangle, 1, 5, default"
+            "fade, 1, 5, default"
+            "workspaces, 1, 5, default"
+          ];
+        };
 
-      # KEYBINDINGS
-      "$mod" = "SUPER";
-      bind = let
-        light = "${pkgs.light}/bin/light";
-        wpctl = "${pkgs.wireplumber}/bin/wpctl";
-        grim = "${pkgs.grim}/bin/grim";
-        slurp = "${pkgs.slurp}/bin/slurp";
-        wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy";
-        playerctl = "${pkgs.playerctl}/bin/playerctl";
-        pkill = "${pkgs.procps}/bin/pkill";
-      in [
-        "$mod, return, exec, ${terminal}"
-        "$mod, Q, killactive,"
-        "$mod, D, exec, ${menu}"
-        "$mod, numbersign, exec, ${browser}"
-        "$mod, Y, exec, ${browser} youtube.com"
-        "$mod_SHIFT, P, exit,"
-        "$mod, B, exec, ${pkill} -SIGUSR1 waybar"
+        master = {
+          new_is_master = true;
+          no_gaps_when_only = 1;
+        };
 
-        # layout commands
-        "$mod_SHIFT, return, layoutmsg, swapwithmaster master"
-        "$mod, tab, cyclenext,"
-        "$mod, space, fullscreen, 1"
-        "$mod_SHIFT, space, fakefullscreen,"
+        misc = {
+          force_default_wallpaper = 0;
+          disable_hyprland_logo = true;
+          new_window_takes_over_fullscreen = 1;
+        };
 
-        # in-workspace movement
-        "$mod, H, movefocus, l"
-        "$mod, J, movefocus, d"
-        "$mod, K, movefocus, u"
-        "$mod, L, movefocus, r"
-        "$mod, F, togglefloating, active"
+        # INPUT
+        input = {
+          kb_layout = "gb";
+          follow_mouse = 1;
+          sensitivity = 0;
 
-        # media keys
-        ", XF86MonBrightnessUp, exec, ${light} -A 5"
-        ", XF86MonBrightnessDown, exec, ${light} -U 5"
-        ", XF86AudioMute, exec, ${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        ", XF86AudioPlay, exec, ${playerctl} play-pause"
-        ", XF86AudioNext, exec, ${playerctl} next"
-        ", XF86AudioPrev, exec, ${playerctl} previous"
-        '', Print, exec, ${grim} -g "$(${slurp} -d)" - | ${wl-copy}''
+          touchpad.natural_scroll = false;
+        };
 
-        # between-workspace movement
-      ] ++ (concatLists (genList (x:
-        let ws = toString (x + 1);
+        gestures = {
+          workspace_swipe = true;
+          workspace_swipe_fingers = 3;
+        };
+
+        # KEYBINDINGS
+        "$mod" = "SUPER";
+        bind = let
+          light = "${pkgs.light}/bin/light";
+          wpctl = "${pkgs.wireplumber}/bin/wpctl";
+          grim = "${pkgs.grim}/bin/grim";
+          slurp = "${pkgs.slurp}/bin/slurp";
+          wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy";
+          playerctl = "${pkgs.playerctl}/bin/playerctl";
+          pkill = "${pkgs.procps}/bin/pkill";
         in [
-          "$mod, ${ws}, workspace, ${ws}"
-          "$mod_SHIFT, ${ws}, moveToWorkspace, ${ws}"
-        ]) 5));
+          "$mod, return, exec, ${terminal}"
+          "$mod, Q, killactive,"
+          "$mod, D, exec, ${menu}"
+          "$mod, numbersign, exec, ${browser}"
+          "$mod, Y, exec, ${browser} youtube.com"
+          "$mod_SHIFT, P, exit,"
+          "$mod, B, exec, ${pkill} -SIGUSR1 waybar"
 
-      binde = let
-        vol-step = toString cfg.keybinds.volume-step;
-        wpctl = "${pkgs.wireplumber}/bin/wpctl";
-      in [
-        ", XF86AudioRaiseVolume, exec, ${wpctl} set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ ${vol-step}%+"
-        ", XF86AudioLowerVolume, exec, ${wpctl} set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ ${vol-step}%-"
-      ];
+          # layout commands
+          "$mod_SHIFT, return, layoutmsg, swapwithmaster master"
+          "$mod, tab, cyclenext,"
+          "$mod, space, fullscreen, 1"
+          "$mod_SHIFT, space, fakefullscreen,"
 
-      bindm =
-        [ "$mod,mouse:272,movewindow" "$mod_SHIFT,mouse:272,resizewindow 2" ];
+          # in-workspace movement
+          "$mod, H, movefocus, l"
+          "$mod, J, movefocus, d"
+          "$mod, K, movefocus, u"
+          "$mod, L, movefocus, r"
+          "$mod, F, togglefloating, active"
 
-      # WORKSPACES
-      workspace = let
-        toLine = (monitor: workspace:
-          let ws = toString workspace;
-          in "${ws},monitor:${monitor}");
-        lists = attrValues (mapAttrs
-          (monitor: workspaces: map (ws: toLine monitor ws) workspaces)
-          cfg.workspaces);
-      in concatLists lists;
+          # media keys
+          ", XF86MonBrightnessUp, exec, ${light} -A 5"
+          ", XF86MonBrightnessDown, exec, ${light} -U 5"
+          ", XF86AudioMute, exec, ${wpctl} set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ", XF86AudioPlay, exec, ${playerctl} play-pause"
+          ", XF86AudioNext, exec, ${playerctl} next"
+          ", XF86AudioPrev, exec, ${playerctl} previous"
+          '', Print, exec, ${grim} -g "$(${slurp} -d)" - | ${wl-copy}''
 
-      # LAYER RULES
-      layerrule = [ "blur,waybar" "blur,rofi" ];
+          # between-workspace movement
+        ] ++ (concatLists (genList (x:
+          let ws = toString (x + 1);
+          in [
+            "$mod, ${ws}, workspace, ${ws}"
+            "$mod_SHIFT, ${ws}, moveToWorkspace, ${ws}"
+          ]) 5));
+
+        binde = let
+          vol-step = toString cfg.keybinds.volume-step;
+          wpctl = "${pkgs.wireplumber}/bin/wpctl";
+        in [
+          ", XF86AudioRaiseVolume, exec, ${wpctl} set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ ${vol-step}%+"
+          ", XF86AudioLowerVolume, exec, ${wpctl} set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ ${vol-step}%-"
+        ];
+
+        bindm =
+          [ "$mod,mouse:272,movewindow" "$mod_SHIFT,mouse:272,resizewindow 2" ];
+
+        # WORKSPACES
+        workspace = let
+          toLine = (monitor: workspace:
+            let ws = toString workspace;
+            in "${ws},monitor:${monitor}");
+          lists = attrValues (mapAttrs
+            (monitor: workspaces: map (ws: toLine monitor ws) workspaces)
+            cfg.workspaces);
+        in concatLists lists;
+
+        # LAYER RULES
+        layerrule = [ "blur,waybar" "blur,rofi" ];
+      };
     };
   };
 }
--- homeManagerModules/display/waybar/default.nix
+++ homeManagerModules/display/waybar/default.nix
@@ -14,6 +14,7 @@ in {
 
     programs.waybar = {
       enable = true;
+      systemd.enable = true;
 
       settings = let sep = "custom/separator";
       in {
