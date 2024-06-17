2514398c0a20574090175cdf488b4e3ca3b8386e
Joshua Smart <josh@thesmarts.co.uk>
2024-06-17 17:17:17 +0100

    Remove redundant gtk desktop portal

--- homeManagerModules/display/default.nix
+++ homeManagerModules/display/default.nix
@@ -4,6 +4,6 @@
 
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-gtk xdg-desktop-portal-wlr ];
+    extraPortals = with pkgs; [ xdg-desktop-portal-wlr ];
   };
 }
