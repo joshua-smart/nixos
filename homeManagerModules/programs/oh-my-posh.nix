661663a696c846838bc6499b58ca55b7a0acd3b4
Joshua Smart <josh@thesmarts.co.uk>
2024-06-28 19:27:51 +0100

    Configured bash prompt

--- homeManagerModules/programs/oh-my-posh.nix
+++ homeManagerModules/programs/oh-my-posh.nix
@@ -1,4 +1,33 @@
-{ ... }: {
+{ ... }:
+let
+  session = {
+    foreground = "green";
+    template = "{{ .UserName }}@{{ .HostName }} ";
+    type = "session";
+  };
+  path = {
+    foreground = "cyan";
+    properties = { style = "folder"; };
+    template = " {{ .Path }} ";
+    type = "path";
+  };
+  git = {
+    foreground = "yellow";
+    properties = {
+      branch_icon = "";
+      fetch_stash_count = true;
+      fetch_status = false;
+      fetch_upstream_icon = true;
+    };
+    template = " ({{ .HEAD }})";
+    type = "git";
+  };
+  sym = {
+    type = "text";
+    foreground = "magenta";
+    template = "$";
+  };
+in {
   programs.oh-my-posh = {
     enable = true;
     enableBashIntegration = true;
@@ -7,54 +36,13 @@
       blocks = [
         {
           alignment = "left";
-          segments = [
-            {
-              background = "#6272a4";
-              foreground = "#f8f8f2";
-              leading_diamond = "";
-              style = "diamond";
-              template = "{{ .UserName }}@{{ .HostName }} ";
-              type = "session";
-            }
-            {
-              background = "#bd93f9";
-              foreground = "#f8f8f2";
-              powerline_symbol = "";
-              properties = { style = "folder"; };
-              style = "powerline";
-              template = " {{ .Path }} ";
-              type = "path";
-            }
-            {
-              background = "#ffb86c";
-              foreground = "#f8f8f2";
-              powerline_symbol = "";
-              properties = {
-                branch_icon = "";
-                fetch_stash_count = true;
-                fetch_status = false;
-                fetch_upstream_icon = true;
-              };
-              style = "powerline";
-              template = "  ({{ .HEAD }}) ";
-              type = "git";
-            }
-          ];
+          segments = [ session path sym ];
           type = "prompt";
         }
         {
           # redundant
           alignment = "left";
-          segments = [{
-            background = "#f1fa8c";
-            foreground = "#282a36";
-            invert_powerline = true;
-            leading_diamond = "";
-            style = "diamond";
-            template = "  {{.Profile}}{{if .Region}}@{{.Region}}{{end}}";
-            trailing_diamond = "";
-            type = "aws";
-          }];
+          segments = [ git ];
           type = "rprompt";
         }
       ];
