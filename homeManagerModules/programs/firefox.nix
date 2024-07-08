aca488f489667c2a1a42486a076006c2663e91ea
Joshua Smart <josh@thesmarts.co.uk>
2024-07-08 18:27:22 +0100

    Added custom search engines to firefox

--- homeManagerModules/programs/firefox.nix
+++ homeManagerModules/programs/firefox.nix
@@ -1,10 +1,39 @@
-{ ... }:
-{
+{ pkgs, ... }: {
   programs.firefox = {
     enable = true;
+
+    profiles = {
+      main = {
+        name = "main";
+        search.engines = {
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [{
+                name = "query";
+                value = "{searchTerms}";
+              }];
+            }];
+            icon =
+              "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@pkgs" ];
+          };
+          "NixOS Wiki" = {
+            urls = [{
+              template = "https://wiki.nixos.org/w/index.php";
+              params = [{
+                name = "search";
+                value = "{searchTerms}";
+              }];
+            }];
+            iconUpdateURL = "https://wiki.nixos.org/favicon.png";
+            updateInterval = 24 * 60 * 60 * 1000;
+            definedAliases = [ "@nixos" ];
+          };
+        };
+      };
+    };
   };
 
-  home.sessionVariables = {
-    MOZ_USE_XINPUT2 = "1";
-  };
+  home.sessionVariables = { MOZ_USE_XINPUT2 = "1"; };
 }
