acad7a358c998c93a0fa29477f5abc74bf8d6e37
Joshua Smart <josh@thesmarts.co.uk>
2024-07-24 00:24:47 +0100

    Added kanata keyboard remapping

--- nixosModules/users.nix
+++ nixosModules/users.nix
@@ -1,10 +1,17 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   users.defaultUserShell = pkgs.zsh;
 
   users.users.js = {
     isNormalUser = true;
     description = "Joshua Smart";
-    extraGroups = [ "networkmanager" "wheel" "video" "docker" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "video"
+      "docker"
+      "uinput"
+    ];
     useDefaultShell = true;
   };
 }
