e90ce884bfba58f75b5e3489601a5fec10f21a2e
Joshua Smart <josh@thesmarts.co.uk>
2024-09-17 12:35:21 +0100

    Modified helix config

--- homeManagerModules/programs/helix.nix
+++ homeManagerModules/programs/helix.nix
@@ -15,8 +15,20 @@ in
       settings = {
         theme = "monokai_pro";
         editor = {
+          completion-trigger-len = 1;
+          completion-replace = true;
           bufferline = "always";
           soft-wrap.enable = true;
+        };
+        keys.insert = {
+          up = "no_op";
+          down = "no_op";
+          left = "no_op";
+          right = "no_op";
+          pageup = "no_op";
+          pagedown = "no_op";
+          home = "no_op";
+          end = "no_op";
         };
       };
 
