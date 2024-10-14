c71a5702ce001af52d30cedf7b3f92b9f21dfdd8
Joshua Smart <josh@thesmarts.co.uk>
2024-10-14 17:26:34 +0100

    Added additional CTRL-<x> bindings to zsh

--- homeManagerModules/programs/zsh.nix
+++ homeManagerModules/programs/zsh.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkIf;
 in
@@ -15,13 +20,16 @@ in
         "ls" = "ls -F --color=auto";
         "la" = "ls -aF";
         "ll" = "ls -alFh";
+        "open" = "${pkgs.xdg-utils}/bin/xdg-open";
       };
 
       dotDir = ".config/zsh";
 
       initExtra = ''
-        bindkey ";5C" forward-word
-        bindkey ";5D" backward-word
+        bindkey "^[[1;5C" forward-word
+        bindkey "^[[1;5D" backward-word
+        bindkey "^H" backward-kill-word
+        bindkey "^[[3;5~" forward-kill-word
       '';
     };
   };
